f6be72517948bfce92087b729c166a56468685bb
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-10-23 14:51:09 +0100

    Fix signature on keyword_handler

--- opgincidentresponse/actions/keyword_handlers.py
+++ opgincidentresponse/actions/keyword_handlers.py
@@ -8,5 +8,5 @@ import logging
 logger = logging.getLogger(__name__)
 
 @keyword_handler(['status page', 'statuspage'])
-def status_page_notification(comms_channel: CommsChannel, user: str, text: str, ts: str):
+def status_page_notification(comms_channel: CommsChannel, user: str, keyword: str, text: str, ts: str):
     comms_channel.post_in_channel(f"ℹ️ You mentioned the Status Page - You can find our statuspage here: https://theofficeofthepublicguardian.statuspage.io/")
