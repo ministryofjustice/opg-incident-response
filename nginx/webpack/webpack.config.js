94c895a3c0651814df213a1382608b17c48dfbc7
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-11-02 11:01:35 +0000

    Move asset compilation to build pipeline

---
+++ nginx/webpack/webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path')
+var CopyPlugin = require('copy-webpack-plugin')
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: {
+    all: './main.js'
+  },
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader?url=false'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build')
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'node_modules/govuk-frontend/govuk/assets', to: path.resolve(__dirname, 'build/assets') },
+        { from: 'node_modules/@ministryofjustice/frontend/moj/assets', to: path.resolve(__dirname, 'build/assets') },
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+  ]
+}
