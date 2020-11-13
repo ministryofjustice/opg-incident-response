eb4abe6b975bd21310004554c5e83f52077d5a2e
Alex Saunders <acsauk@gmail.com>
2020-11-13 16:25:11 +0000

    Reinstate function signature and use correct keywords for runbooks

--- opgincidentresponse/actions/keyword_handlers.py
+++ opgincidentresponse/actions/keyword_handlers.py
@@ -4,9 +4,9 @@ from response.slack.models import CommsChannel
 from response.slack.decorators import keyword_handler
 
 @keyword_handler(['status page', 'statuspage'])
-def status_page_notification(comms_channel: CommsChannel):
+def status_page_notification(comms_channel: CommsChannel, user: str, keyword: str, text: str, ts: str):
     comms_channel.post_in_channel(f"ℹ️ You mentioned the Status Page - You can find our statuspage here: https://theofficeofthepublicguardian.statuspage.io/")
 
-@keyword_handler(['status page', 'statuspage'])
-def runbook_notification(comms_channel: CommsChannel):
+@keyword_handler(['runbook', 'run book'])
+def runbook_notification(comms_channel: CommsChannel, user: str, keyword: str, text: str, ts: str):
     comms_channel.post_in_channel(f"ℹ️ You mentioned runbooks - You can find runbooks for our services here: https://ministryofjustice.github.io/opg-technical-guidance/#opg-technical-guidance/")
--- opgincidentresponse/tests/actions/test_keyword_handlers.py
+++ opgincidentresponse/tests/actions/test_keyword_handlers.py
@@ -5,13 +5,13 @@ from response.slack.models import CommsChannel
 def test_status_page_notification(mocker):
     mocker.patch.object(CommsChannel, 'post_in_channel')
     
-    status_page_notification(CommsChannel)
+    status_page_notification(CommsChannel, '', '', '', '')
 
     CommsChannel.post_in_channel.assert_called_once_with("ℹ️ You mentioned the Status Page - You can find our statuspage here: https://theofficeofthepublicguardian.statuspage.io/")
 
 def test_runbook_notification(mocker):
     mocker.patch.object(CommsChannel, 'post_in_channel')
     
-    runbook_notification(CommsChannel)
+    runbook_notification(CommsChannel, '', '', '', '')
 
     CommsChannel.post_in_channel.assert_called_once_with("ℹ️ You mentioned runbooks - You can find runbooks for our services here: https://ministryofjustice.github.io/opg-technical-guidance/#opg-technical-guidance/")
