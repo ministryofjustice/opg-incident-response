b67719dfd990499f8d6692a8274645654654400a
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2020-12-04 14:12:47 +0000

    Handle links and references more effectively
    Now works consistently across actions, descriptions and timeline and avoids nasty HTML

--- opgincidentresponse/templatetags/opg_response.py
+++ opgincidentresponse/templatetags/opg_response.py
@@ -11,6 +11,8 @@ def slack_format(value):
     if type(value) is not str:
         return value
 
+    value = value.replace('<', '&lt;').replace('>', '&gt;')
+
     user_links = re.findall(r"&lt;@(U.+?)&gt;", value)
 
     for user_id in user_links:
@@ -18,11 +20,11 @@ def slack_format(value):
 
         value = value.replace('&lt;@' + user_id + '&gt;', '<a class="govuk-link" href="slack://user?team=T02DYEB3A&amp;id=' + user.external_id + '">' + user.full_name + '</a>')
 
-    links = re.findall(r"<(https?://.+?)(?:\|(.+?))?>", value)
+    links = re.findall(r"&lt;(https?://.+?)(?:\|(.+?))?&gt;", value)
 
     for (link, label) in links:
         match = (link + '|' + label) if label else link
-        value = value.replace('<' + match + '>', '<a class="govuk-link" href="' + link + '">' + (label if label else link) + '</a>')
+        value = value.replace('&lt;' + match + '&gt;', '<a class="govuk-link" href="' + link + '">' + (label if label else link) + '</a>')
 
     return value.replace('\n', '<br />')
 
