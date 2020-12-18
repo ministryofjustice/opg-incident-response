8a29b38e78af5fe3453b5a02dec66e4606cc0b4f
Tom Withers <thomas.withers@digital.justice.gov.uk>
2020-12-18 13:59:06 +0000

    Import the standard response notifications

--- opgincidentresponse/actions/incident_notifications.py
+++ opgincidentresponse/actions/incident_notifications.py
@@ -1,14 +1,20 @@
 from response.core.models import Incident
 from response.slack.models import CommsChannel
 from response.slack.decorators import recurring_notification, single_notification
-
+from response.slack import incident_notifications
 
 @single_notification()
 def incident_response_process(incident: Incident):
-    comms_channel = CommsChannel.objects.get(incident=incident)
-    comms_channel.post_in_channel("📗 You can find our Incident Response process here https://ministryofjustice.github.io/opg-technical-guidance/incidents/incident-response-process/#incident-response-process")
+    try:
+        comms_channel = CommsChannel.objects.get(incident=incident)
+        comms_channel.post_in_channel("📗 You can find our Incident Response process here https://ministryofjustice.github.io/opg-technical-guidance/incidents/incident-response-process/#incident-response-process")
+    except CommsChannel.DoesNotExist:
+        pass
 
 @recurring_notification(interval_mins=30, max_notifications=10)
 def take_a_break(incident: Incident):
-    comms_channel = CommsChannel.objects.get(incident=incident)
-    comms_channel.post_in_channel("👋 30 minutes have elapsed. Think about taking a few minutes away from the screen.")
+    try:
+        comms_channel = CommsChannel.objects.get(incident=incident)
+        comms_channel.post_in_channel("👋 30 minutes have elapsed. Think about taking a few minutes away from the screen.")
+    except CommsChannel.DoesNotExist:
+        pass
