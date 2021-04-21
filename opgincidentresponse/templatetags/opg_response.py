2cc9263ae55786858796efc3c1de693929dacd58
Greg Tyler <gregtyler@users.noreply.github.com>
2021-04-21 08:52:51 +0100

    Allow editing of actions (#77)
    * Remove unused CSS files
    
    * Add fallback if referenced user isn't in database
    
    * Add basic functionality for editing an action
    
    * Match user tags based on ID, user- or display name
    
    This allows you to tag people in actions with e.g. <@Greg Tyler> rather than needing to know their user ID

--- opgincidentresponse/templatetags/opg_response.py
+++ opgincidentresponse/templatetags/opg_response.py
@@ -2,6 +2,7 @@ import re
 
 from django import template
 from django.conf import settings
+from django.db.models import Q
 from response.core.models import ExternalUser
 
 register = template.Library()
@@ -13,10 +14,15 @@ def slack_format(value):
 
     value = value.replace('<', '&lt;').replace('>', '&gt;')
 
-    user_links = re.findall(r"&lt;@(U.+?)&gt;", value)
+    user_links = re.findall(r"&lt;@(.+?)&gt;", value)
 
     for user_id in user_links:
-        user = ExternalUser.objects.get(external_id=user_id)
+        try:
+            user = ExternalUser.objects.get(
+                Q(external_id=user_id) | Q(display_name__iexact=user_id) | Q(full_name__iexact=user_id)
+            )
+        except:
+            user = ExternalUser(external_id=user_id, full_name='@' + user_id)
 
         value = value.replace('&lt;@' + user_id + '&gt;', '<a class="govuk-link" href="slack://user?team=T02DYEB3A&amp;id=' + user.external_id + '">' + user.full_name + '</a>')
 
@@ -27,6 +33,10 @@ def slack_format(value):
         value = value.replace('&lt;' + match + '&gt;', '<a class="govuk-link" href="' + link + '">' + (label if label else link) + '</a>')
 
     return value.replace('\n', '<br />')
+
+@register.filter
+def escape_quote(value):
+    return value.replace('"', '&quot;')
 
 @register.filter
 def slack_dm_link(id):
