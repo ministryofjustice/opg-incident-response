e23f3dd4fb82871c67a576a90b2f45c51746074e
Andy Price <sixdaysandy@gmail.com>
2024-04-24 16:50:29 +0100

    SP-2061 - Fix ALB Bucket S3 Permissions, wait for ECS Service to be stable #minor

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -3,13 +3,14 @@ resource "aws_ecs_cluster" "cluster" {
 }
 
 resource "aws_ecs_service" "service" {
-  name             = "response"
-  cluster          = aws_ecs_cluster.cluster.id
-  task_definition  = aws_ecs_task_definition.response.arn
-  desired_count    = 1
-  launch_type      = "FARGATE"
-  platform_version = "1.4.0"
-  depends_on       = [aws_lb.loadbalancer]
+  name                  = "response"
+  cluster               = aws_ecs_cluster.cluster.id
+  task_definition       = aws_ecs_task_definition.response.arn
+  desired_count         = 1
+  launch_type           = "FARGATE"
+  platform_version      = "1.4.0"
+  depends_on            = [aws_lb.loadbalancer]
+  wait_for_steady_state = true
 
   deployment_controller {
     type = "ECS"
