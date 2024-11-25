77b532f42563bfdffc3ea8ff2f522970e44fb513
Matt <matt.machell@digital.justice.gov.uk>
2024-11-25 16:23:17 +0000

    Revert "Remove old dep (#305)" (#315)
    This reverts commit bdbd7ad71aa50a257f1bdedfdd30f160f472f2ca.

--- opgincidentresponse/settings/base.py
+++ opgincidentresponse/settings/base.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "after_response",
     "bootstrap4",
     "response.apps.ResponseConfig",
     "rest_framework",
