2750e1b94fc364667d17bcb1725935feae3cdf39
Matt <Matt.Machell@publicguardian.gov.uk>
2026-01-05 14:44:20 +0000

    Remove testcfg prod and make fake secret more obvious

--- testcfg/settings/base.py
+++ testcfg/settings/base.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "5m*pc(gb!k+!913@qh2u5z7^s-bbp2ytw28gdvcbly5ayw1i5+"
+SECRET_KEY = "ThisIsAFakeSecret-1234$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
