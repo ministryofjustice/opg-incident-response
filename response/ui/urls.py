18c88f0c158ac273bc2a0e10e420188684330abf
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2026-06-18 13:46:32 +0100

    Replace bleach with justhtml
    This ensures any submitted HTML is sanitised, and removes some confusing files that are no longer used.
    
    It transpires that we don't actually use markdown_filter to convert markdown to HTML, so all of that functionality can be removed.
    
    - Reinstate `.in` files to manage direct dependencies
    - Install justhtml, remove bleach and markdown2, update requirements files
    - Remove unused templates in `./response` (they're overridden by `./opgincidentresponse`)
    - Remove `MARKDOWN_FILTER_` settings
    - Use justhtml rather than bleach when sanitizing user input
    - Fix the mounted files in docker-compose.yml
    
    #patch

--- response/ui/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("incident/<int:incident_id>/", views.incident_doc, name="incident_doc"),
-]
